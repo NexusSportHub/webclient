d7f5426885f7573bba37485f24e41f0aff9469c7
unknown <noeliatinajero1996@gmail.com>
2023-12-21 14:48:40 +0100

    creamos y subimos el proyecto maven

---
+++ NexusSportHub/src/test/java/com/NexusSportHub/NexusSportHub/NexusSportHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NexusSportHub.NexusSportHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusSportHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
